00ca14165a0890bb6c170e70fcacfde7be131a8a
mmonster <496268609@qq.com>
2017-06-08 16:14:30 +0800

    注释修改

--- netty/HelloNetty4/src/main/java/org/goskyer/timeserver/TimeClient.java
+++ netty/HelloNetty4/src/main/java/org/goskyer/timeserver/TimeClient.java
@@ -45,7 +45,7 @@ public class TimeClient {
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             new Thread(() -> {
                 try {
                     Thread.sleep(2000);
